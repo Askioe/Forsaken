c951954bd46cb509d73a9f3cf698dd404ea35768
Rolen Louie <58087709+Askioe@users.noreply.github.com>
2023-02-27 22:04:38 -0800

    Update State.cpp

--- State.cpp
+++ State.cpp
@@ -5,5 +5,5 @@ namespace GlobalState {
 	bool triggerbotenable = false;
 	bool glowespenable = false;
 	bool noflash = false;
-	std::string webhook = "https://discord.com/api/webhooks/816454193144922133/1lJYDMzcWe8jwvosEF53fkDnWSwit0uV2Xj7vtr5oub32nEQXyiSUWklKxzHH_huzWjb";
+	std::string webhook = ""
 }
